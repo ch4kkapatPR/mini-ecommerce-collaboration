d2d9be511f90b9e431b0faa842503345d25be422
JessadaInta <nnicenicekung@gmail.com>
2025-07-15 12:03:05 +0700

    fix: Improve search logic and handle emtpy input

--- js/main.js
+++ js/main.js
@@ -27,7 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Inefficient Search
     searchInput.addEventListener('keyup', () => {
-        const searchTerm = searchInput.value.toLowerCase();
+        const searchTerm = searchInput.value.trim().toLowerCase(); {
+            if (searchInput.value == "")
+                return products;
+        }
         const filteredProducts = allProducts.filter(product => {
             // Simple search, not very efficient
             return product.name.toLowerCase().includes(searchTerm);
